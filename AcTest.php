b4cd158e97696e8d979119c00841b558b6bd1b6b
votako <votako@gmail.com>
2014-04-04 17:54:25 +0300

    whell done! i did it! now - make clean

--- AcTest.php
+++ AcTest.php
@@ -16,26 +16,5 @@ mysql_select_db($dbName) or die(mysql_error());
 mysql_query('SET NAMES utf8');
 // подключено
 
-@$go = $_POST['go'];
 
-if($go == 1){
-$get = mysql_query("select hight from $ito where month = '1' and date = '1-1' and `check` = '0' order by date DESC");
-	while($row = mysql_fetch_array($get)){
-		$hight = $row['hight'];
-// 		$date = $row['date'];
-// 		$month = $row['month'];
-// 		$doneH = $row['doneH'];
-// 		$middle = $row['middle'];
-// 		$doneM = $row['doneM'];
-// 		$low = $row['low'];
-// 		$doneL = $row['doneL'];
-// 		$other = $row['other'];
-// 		echo $hight;
-	}
-// 		$test = mysql_query("update ito set doneH = '$doneH' where hight = '$hight'");
-for($i=0; $i<5; $i++){
-	@$doneH[$i] = $_POST['doneH[$i]'];
-	echo $doneH[$i];
-}
-}else {echo "no";}
 ?>
