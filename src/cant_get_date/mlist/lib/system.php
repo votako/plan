cde073bae74a93b0ea4abdb35ad65977a46102e6
votako <votako@gmail.com>
2014-04-07 17:21:08 +0300

    make clean

--- src/cant_get_date/mlist/lib/system.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require_once('template.php');
-  require_once('config.php');
-  require_once('db.php');
-  require_once('functions.php');
-  //session_set_cookie_params(120);
-  ini_set('session.gc_maxlifetime','120');
-  session_start();
-?>
